3b30bac9f6b6a98825354cb8e649777d9dd93f3d
WayLeung <610686949@qq.com>
2018-10-09 23:46:57 +0800

    add thread pool

---
+++ src/com/way/t2/Demo6.java
@@ -0,0 +1,29 @@
+package com.way.t2;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Demo6 {
+    public static void main(String[] args) {
+        //固定的线程池
+        //ExecutorService threadPool = Executors.newFixedThreadPool(10);
+
+        ExecutorService threadPool = Executors.newCachedThreadPool();
+
+
+
+
+        for (int i=0;i<100;i++) {
+            threadPool.execute(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println(Thread.currentThread().getName());
+                }
+            });
+        }
+
+        threadPool.shutdown();
+
+    }
+}
